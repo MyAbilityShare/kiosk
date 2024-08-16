63ed49a7669c670691374bcdfdd9573df3ec6faf
An minsun <elleto123456@gmail.com>
2024-08-16 09:23:32 +0900

    [rename] 폴더 정리

---
+++ kiosk/JS/total_price.js
@@ -0,0 +1,11 @@
+let totalAmount = 0;
+    const menuItems = document.querySelectorAll('.img');
+    const selectedItems = document.getElementById('selected-items');
+
+    menuItems.forEach(item => {
+      item.addEventListener('click', () => {
+        const price = parseInt(item.getAttribute('data-price')); // data-price 속성에서 가격을 가져옵니다
+        totalAmount += price; // totalAmount에 가격을 누적합니다
+        selectedItems.innerHTML = `<span id="total-amount">${totalAmount}원</span><br><span id="payment-text">결제하기</span>`; // 'value' 속성을 사용하여 버튼의 텍스트를 변경합니다
+      });
+    });
